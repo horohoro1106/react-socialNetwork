1acce953bbc09edece7c1439eeefd418e3597c37
horohoro1106 <horohoro1106@gmail.com>
2019-01-16 20:52:16 +0200

    first commit

---
+++ src/components/Profie.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+        <div>
+        <img src="https://nroer.gov.in/media/e/a/c/c4ce9ba9211d05e48f0bf447a346dde5f0c79a31c163d540ce9c282b43138.jpeg" alt="cover-img" />
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          my posts
+          <div>
+            new post
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+          </div>
+        </div>
+        </div>
+    )
+}
+
+export default Profile;
