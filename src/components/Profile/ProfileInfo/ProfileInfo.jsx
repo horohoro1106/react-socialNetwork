9bc27ab42cc3546f2307b588171631704117a883
horohoro1106 <horohoro1106@gmail.com>
2019-03-10 11:07:22 +0200

    added router

---
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import classes from './ProfileInfo.module.css';
+
+const ProfileInfo = (props) => {
+    return (
+        <div>
+            <div>
+                <img src="http://www.trendycovers.com/covers/abstract_3d_facebook_cover_1370594397.jpg"
+                     alt="cover-img"/>
+            </div>
+            <div>
+                ava + description
+            </div>
+        </div>
+    )
+}
+
+export default ProfileInfo;
